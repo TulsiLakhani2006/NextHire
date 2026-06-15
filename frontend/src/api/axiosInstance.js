31eeb92caf1d5a8dfdbdd65d9e0512157f899899
Tulsi Lakhani <tulsi.lakhani03@gmail.com>
2026-06-16 00:44:44 +0530

    Fix API URL for production

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: "http://localhost:8080/api" })
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL + "/api"
+})
 
 api.interceptors.request.use((config) => {
   const stored = localStorage.getItem('auth')
